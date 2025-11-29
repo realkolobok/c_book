ad8caf6b6fd0fea7a4ca06aa6f5c88597facf887
realkolobok <kolobokkitajskij@gmail.com>
2025-11-29 21:15:54 +0200

    Add 1-20

--- 1_9/exercises/1_20/detab.c
+++ 1_9/exercises/1_20/detab.c
@@ -1,39 +1,21 @@
 #include <stdio.h>
 
-#define TABSIZE 8
-
-void getline(char line[], int size);
-void detab(char line[]);
-void copy(char from[], char to[]);
+#define TAB_LENGTH 8
 
 int main()
 {
-	return 0;
-}
+    int c, spaces;
 
-int getline(char line[], int size)
-{
-    int c, i;
-    for (i = 0; i < size-1 && (c = getchar()) != EOF && c != '\n'; ++i)
-        s[i] = c;
-    if (c == '\n') {
-        s[i] = '\n';
-        ++i;
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            spaces = TAB_LENGTH;
+            while (spaces) {
+                putchar(' ');
+                --spaces;
+            }
+        } else {
+            putchar(c);
+        }
     }
-    s[i] = '\0';
-    return i;
-}
-
-void detab(char line[])
-{
-	int col, spaces, n;
-	n = TABSIZE;
-}
-
-void copy(char from[], char to[])
-{
-    int i;
-    for (i = 0; from[i] != '\0'; ++i)
-        to[i] = from[i];
-    to[i] = '\0';
+    return 0;
 }
