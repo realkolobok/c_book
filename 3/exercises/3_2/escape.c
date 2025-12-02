cf76376b340ee6e397e4e845ed070ea4883ac7e8
realkolobok <kolobokkitajskij@gmail.com>
2025-12-02 08:04:50 +0200

    Start 3-2

---
+++ 3/exercises/3_2/escape.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define ARRAYLEN 1000
+
+void escape(char s[], char t[]);
+void unescape(char s[], char t[]);
+
+int main()
+{
+    return 0;
+}
+
+void escape(char s[], char t[])
+{
+    int i, j;
+    for (i = j = 0; t[i] != '\0'; i++, j++) {
+        switch (t[i]) {
+            case '\t':
+                s[j] = '\\';
+                s[++j] = 't';
+                break;
+            case '\n':
+                s[j] = '\\';
+                s[++j] = 'n';
+                break;
+            case '\b':
+                s[j] = '\\';
+                s[++j] = 'b';
+                break;
+            case '\r':
+                s[j] = '\\';
+                s[++j] = 'r';
+                break;
+            case '\v':
+                s[j] = '\\';
+                s[++j] = 'v';
+                break;
+            default:
+                s[j] = t[i];
+                break;
+        }
+    }
+    s[j] = '\0';
+}
